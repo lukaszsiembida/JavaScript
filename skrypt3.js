01c8b2e4d6c8a72d166f064bc0c02ac3030944a6
lukasz <lukaszsiembida@wp.pl>
2020-09-27 15:29:09 +0200

    zadanie javascript Formularz hidden warning

--- skrypt3.js
+++ skrypt3.js
@@ -5,13 +5,35 @@ function validateForm(formName){
    //      return false;
    //  }
 
+    let isValid = true;
     let form = document.forms[formName];
     for(let formElement of form){
         if(formElement.tagName.toLowerCase() === 'input'){
             if(formElement.value === ''){
-                alert(formElement.parentElement.innerText + ' musi zostać wypełnione.');
-                return false;
+                changeStyleToWarning(formElement);
+                isValid = false;
+            } else {
+                formElement.style = "";
             }
         }
     }
+    if(!isValid){
+        showWarning();
+    } else {
+        hideWarning();
+    }
+    return isValid;
+}
+
+function changeStyleToWarning(formElement){
+    formElement.style = 'background-color: #ffe3f3; border-color: red';
+    //alert(formElement.parentElement.innerText + ' musi zostać wypełnione.');
+}
+
+function showWarning(){
+    document.getElementById('warning').removeAttribute('hidden');
+}
+
+function hideWarning() {
+    document.getElementById('warning').setAttribute('hidden','')
 }
