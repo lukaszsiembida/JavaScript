702f0dc5c48c96c2e35ec2d786f3aff4aec0e561
lukasz <lukaszsiembida@wp.pl>
2020-09-27 13:14:53 +0200

    zadanie 13 javascript

--- skrypt.js
+++ skrypt.js
@@ -1,2 +1,25 @@
-alert("Hello World!");
-console.log("hello world!");
+//zadanie 12
+// alert("Hello World!");
+// console.log("hello world!");
+
+//zadanie 13
+let var1 = 1;
+let var2 = 1;
+if(var1 == var2) {
+    console.log('var1 == var2');
+}
+if(var1 === var2) {
+    console.log('var1 === var2');
+}
+
+var1 = 1;
+var2 = '1';
+
+if(var1 == var2) {
+    console.log('var1 == var2');
+}
+if(var1 === var2) {
+    console.log('var1 === var2');
+} else {
+    console.log("var1 !== var2")
+}
